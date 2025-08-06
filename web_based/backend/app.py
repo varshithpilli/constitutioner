7d3de40a8a40c6c200b01948883a807f3fd93d0b
varshith pilli <varshith6946@gmail.com>
2025-08-06 11:52:53 +0530

    deploy3

--- web_based/backend/app.py
+++ web_based/backend/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from deploy_model import Constitutioner
+from .deploy_model import Constitutioner
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 
